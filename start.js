677ec19e30b586986b7d5d2a9f4b9684180c5423
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-09-15 18:30:04 -0700

    Adds trending tags request

--- start.js
+++ start.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var d3 = require('d3');
+var request = require('request');
 
 var json = require('./JSON/post-labels.json');
 
@@ -19,6 +20,21 @@ var databaseUrl = "http://10.14.41.30:8081/label/";
  */
 app.get('/', function (req, res) {
 	res.sendFile('index.html');
+})
+
+/*
+ * Request trending tags
+ */
+request.post({
+	headers: {'content-type' : 'application/json; charset=utf-8'},
+	url: 'http://10.14.41.30:8081/api/labels/top',
+	json: true,
+	body: {
+		"start_time": 1505258232,
+		"end_time": 1505517432
+	}
+}, function(err, res, body) {
+	// console.log(body);
 })
 
 /*
