1f59511022c70dc63619001770690c39c38d5780
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-09-20 10:46:38 -0700

    Change port to 8081

--- start.js
+++ start.js
@@ -111,7 +111,7 @@ request.post({
 /*
  * Express app
  */
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8081, function(){
 	console.log("Tada server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
