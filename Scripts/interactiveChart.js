87bb83998a34617729bad3bf391d97e4617a7e5c
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-09-24 15:12:39 -0700

    Init Google chart on interactive chart

--- Scripts/interactiveChart.js
+++ Scripts/interactiveChart.js
@@ -1,25 +1,40 @@
 
 // Loads google packages
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawTooltipsCharts);
+google.charts.setOnLoadCallback(drawChart);
 
 // Set up data for main chart
 var primaryData = JSON.parse(document.getElementById('raw-data').getAttribute('data-data'));
-primaryData = primaryData.slice(0,30);
+// primaryData = primaryData.slice(0,30);
+console.log(primaryData)
 document.getElementById('raw-data').removeAttribute('data-data');
 
 // Set up data for tooltips
 var tooltipData = JSON.parse(document.getElementById('raw-data').getAttribute('data-sub'));
 document.getElementById('raw-data').removeAttribute('data-data');
 
+//Draw chart
+function drawChart() {
+	var dataTable = new google.visualization.DataTable();
+	dataTable.addColumn('string', 'description');
+	dataTable.addColumn('number', 'score');
+	
+	dataTable.addRows(primaryData);
 
-// Draws the charts to pull PNG for tooltips
-// function drawTooltipsCharts() {
-// 	var data = new google.visualization.arrayToDataTable(tooltipData);
-// 	var view = new google.visualization.DataView(data);
+	var options = {
+		'title' : 'Top objects in Sims Tumblr posts',
+		'width' : 960,
+		'height' : 1000,
+		'chartArea' : {
+			'width' : '60%',
+			'height' : '80%'
+		}
+	};
 
-// 	for (var i = 0; i < primaryData.length; i++)
-// }
+	var chart = new google.visualization.BarChart(document.getElementById('chart_main'));
+	chart.draw(dataTable, options);
+}
+
 
 console.log(primaryData);
 console.log(tooltipData);
